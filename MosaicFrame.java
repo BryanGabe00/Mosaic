1f2e4d1fc41093022920bdd13e78030cb50343ea
BryanGabe00 <bryantgabe@lewisu.edu>
2019-03-04 22:31:26 -0600

    Finished v1 the tiles load on a screen

--- MosaicFrame.java
+++ MosaicFrame.java
@@ -10,10 +10,12 @@ import java.awt.Container;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 public class MosaicFrame extends JFrame
 {
   private final double VERSION = 1.0;
+  private ArrayList<XAndOTile> tiles;
   public MosaicFrame()
   {
     setTitle("Mosaic " + VERSION);
@@ -32,5 +34,11 @@ public class MosaicFrame extends JFrame
     JPanel xAndOPanel = new JPanel();
     contentPane.add(xAndOPanel, BorderLayout.CENTER);
     xAndOPanel.setLayout(new GridLayout(3, 3));
+
+    tiles = new ArrayList<XAndOTile>();
+    for(int i = 0; i < 9; i++)
+    {
+      xAndOPanel.add(new XAndOTile());
+    }
   }
 }
--- XAndOTile.java
+++ XAndOTile.java
@@ -2,7 +2,6 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Font;
-import java.util.ArrayList;
 import java.util.Random;
 
 public class XAndOTile extends JPanel
@@ -13,5 +12,36 @@ public class XAndOTile extends JPanel
   public XAndOTile()
   {
     super();
+    setRandomValues();
+  }
+  final public void setRandomValues()
+  {
+    red = getRandomNumber(0, 255);
+    green = getRandomNumber(0, 255);
+    blue = getRandomNumber(0, 255);
+
+    letter = "X";
+    if(getRandomNumber(0, 1) == 1)
+      letter = "O";
+  }
+  private int getRandomNumber(int min, int max)
+  {
+    Random r = new Random();
+    return r.nextInt(max - min + 1) + min;
+  }
+  private int getContrastingColor(int color)
+  {
+    return (color + 128) % 256;
+  }
+  public void paintComponent(Graphics g)
+  {
+    super.paintComponent(g);
+    final int FONT_SIZE = 100;
+    g.setColor(new Color(red, green, blue));
+    g.fillRect(10, 10, getWidth() - 20, getHeight() - 20);
+    g.setColor(new Color(getContrastingColor(red), getContrastingColor(green), getContrastingColor(blue)));
+
+    g.setFont(new Font("TimesRoman", Font.PLAIN, FONT_SIZE));
+    g.drawString(letter, getWidth() / 2 - 30, getHeight() / 2 + 30);
   }
 }
